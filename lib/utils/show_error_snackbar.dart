9fd5d096b108c4c940a7664b68234edd9175f1ba
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2023-02-25 14:20:26 -0500

    Update show_error_snackbar.dart

--- lib/utils/show_error_snackbar.dart
+++ lib/utils/show_error_snackbar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-void showErrorSnackbar(BuildContext context, String text) {
+void showErrorSnackbar(BuildContext context, String text,
+    {int durationInSeconds = 5}) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
     content: Text(text),
     backgroundColor: Colors.red.shade400,
+    duration: Duration(seconds: durationInSeconds),
   ));
 }
