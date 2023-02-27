61067bd9289136adea0d80e451645e26d47f0a76
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2023-02-26 19:57:27 -0500

    inited firebase_auth_provider

---
+++ lib/services/auth/firebase_auth_provider.dart
@@ -0,0 +1,36 @@
+import 'package:myapp/services/auth/auth_user.dart';
+
+import 'auth_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
+
+class FirebaseAuthProvider implements AuthProvider {
+  @override
+  Future<AuthUser> createUser(
+      {required String email, required String password}) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser get currentUser => throw UnimplementedError();
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+}
